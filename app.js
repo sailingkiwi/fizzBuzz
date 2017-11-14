55c226594c1cf6c7ec1d1ec10f588b434a284740
Ritsu <sailingkiwi@gmail.com>
2017-11-13 20:02:34 -1000

    fizzbuzz

--- app.js
+++ app.js
@@ -4,3 +4,12 @@ if the number is a multiple of 3, it will console.log "fizz",
 if the number is a multiple of 5, it will console.log "buzz",  
 if the number is a multiple of 3 and 5, it will console.log "fizzBuzz"
 */
+for (var i=1; i<=100; i++){
+  if(i%3 === 0 && i%5 === 0){
+    console.log("fizz buzz");
+  }else if(i%3 === 0){
+    console.log("fizz");
+  }else if(i%5 === 0){
+    console.log("buzz");
+  }
+}
